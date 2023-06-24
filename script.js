21406f0fa8768b7832102732e5d152ce5fa065a1
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-06-24 11:54:32 +0300

    Zoom on images on hover

--- script.js
+++ script.js
@@ -2,20 +2,42 @@
 const titleZoom = document.querySelector('.title-zoom');
 const titleElements = titleZoom.querySelectorAll('h1');
 
-titleElements.forEach(function(element) {
+titleElements.forEach(function (element) {
     element.style.transition = 'transform 0.3s ease';
 });
 
-titleZoom.addEventListener('mouseover', function() {
-    titleElements.forEach(function(element) {
+titleZoom.addEventListener('mouseover', function () {
+    titleElements.forEach(function (element) {
         element.style.transformOrigin = 'top left';
         element.style.transform = 'scale(1.3)';
     });
 });
 
-titleZoom.addEventListener('mouseout', function() {
-    titleElements.forEach(function(element) {
+titleZoom.addEventListener('mouseout', function () {
+    titleElements.forEach(function (element) {
         element.style.transformOrigin = 'top left';
+        element.style.transform = 'scale(1)';
+    });
+});
+
+
+// ---- Zoom sur les images ----
+const imgElements = document.querySelectorAll('.image-zoom');
+
+imgElements.forEach(function (element) {
+    element.style.transition = 'transform 0.3s ease';
+});
+
+imgElements.forEach(function (element) {
+    element.addEventListener('mouseover', function () {
+        element.style.transformOrigin = 'center';
+        element.style.transform = 'scale(1.1)';
+    });
+});
+
+imgElements.forEach(function (element) {
+    element.addEventListener('mouseout', function () {
+        element.style.transformOrigin = 'center';
         element.style.transform = 'scale(1)';
     });
 });
