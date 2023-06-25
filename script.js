6c38ef214cbf7948502102cd28cadab413ed2747
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-06-25 19:20:53 +0300

    logo BP souris

--- script.js
+++ script.js
@@ -76,3 +76,32 @@ window.addEventListener('scroll', handleScrollAnimation);
 window.addEventListener('resize', handleScrollAnimation);
 window.addEventListener('load', handleScrollAnimation);
 
+// Souris
+const logo = document.getElementById('black-panther-logo');
+
+// Fonction pour mettre à jour la position du logo en fonction de la souris et de la position de défilement
+function updateLogoPosition(event) {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    const scrollX = window.pageXOffset;
+    const scrollY = window.pageYOffset;
+
+    logo.style.transform = `translate(${mouseX + scrollX}px, ${mouseY + scrollY}px)`;
+}
+
+// Fonction pour afficher le logo avec un effet fondu
+function fadeInLogo() {
+    logo.style.opacity = '1';
+}
+
+// Fonction pour masquer le logo avec un effet fondu
+function fadeOutLogo() {
+    logo.style.opacity = '0';
+}
+
+// Écouteur d'événement pour suivre le mouvement de la souris et mettre à jour la position du logo
+document.addEventListener('mousemove', updateLogoPosition);
+
+// Écouteurs d'événements pour afficher/masquer le logo lorsqu'il entre/sort de la fenêtre
+document.addEventListener('mouseenter', fadeInLogo);
+document.addEventListener('mouseleave', fadeOutLogo);
